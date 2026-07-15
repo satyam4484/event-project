25ebe867ff1f382153822e58b449d85806f6b731
satyam4484 <satyambsingh93@gmail.com>
2026-07-15 20:36:54 +0530

    Add GitHub Pages deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/event-project/',
   build: {
     chunkSizeWarningLimit: 1200,
     rollupOptions: {
